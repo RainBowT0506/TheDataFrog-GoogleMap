b4182863325c76047bb10679f346a113bd3af3d4
RainBowT <rainbowt0506@gmail.com>
2024-01-01 20:52:56 +0800

    feat: Add function to plot an interactive Google Map with Custom Hover Tool
    - Created a new function 'plotType5' to generate an interactive Bokeh plot with Google Maps integration
    - The function allows specifying latitude, longitude, zoom level, and map type (default: roadmap)
    - Added a custom Hover Tool displaying tooltips for price, building area, and terrain area
    - Used a ColumnDataSource to handle the data points from the dataframe
    - Plotted yellow circles on the map at the specified latitude and longitude from the dataframe
    - Displayed the interactive plot with the custom Hover Tool

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from Constant import API_Key_Google_Map
 from bokeh.io import output_file, show
 from bokeh.plotting import gmap
 from bokeh.models import GMapOptions,ColumnDataSource
+from bokeh.models import HoverTool
+
 
 bokeh_width, bokeh_height = 500, 400
 
@@ -64,13 +66,38 @@ def plotType4(lat, lng, zoom=10, map_type='roadmap'):
     show(p)
     return p
 
+def plotType5(lat, lng, zoom=10, map_type='roadmap'):
+    gmap_options = GMapOptions(lat=lat, lng=lng,
+                               map_type=map_type, zoom=zoom)
+    # the tools are defined below:
+    hover = HoverTool(
+        tooltips = [
+            # @price refers to the price column
+            # in the ColumnDataSource.
+            ('price', '@price euros'),
+            ('building', '@area_build m2'),
+            ('terrain', '@area_tot m2'),
+        ]
+    )
+    # below we replaced 'hover' (the default hover tool),
+    # by our custom hover tool
+    p = gmap(API_Key_Google_Map, gmap_options, title='Pays de Gex',
+             width=bokeh_width, height=bokeh_height,
+             tools=[hover, 'reset', 'wheel_zoom', 'pan'])
+    source = ColumnDataSource(df)
+    center = p.circle('lon', 'lat', size=4, alpha=0.5,
+                      color='yellow', source=source)
+    show(p)
+    return p
+
 # p = plotType1(lat, lng)
 #
 # p = plotType2(lat, lng, map_type='terrain')
 #
 # p = plotType3(lat, lng, map_type='satellite')
 
-p = plotType4(lat, lng, map_type='satellite', zoom=12)
+# p = plotType4(lat, lng, map_type='satellite', zoom=12)
 
-show(p)
+p = plotType5(lat, lng, map_type='satellite', zoom=12)
+
 
