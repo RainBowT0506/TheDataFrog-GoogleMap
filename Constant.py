4de790895f8058effa19b093135d748e080812f8
RainBowT <rainbowt0506@gmail.com>
2024-01-01 19:36:30 +0800

    feat: Create a Bokeh plot with Google Maps integration using pandas data
    - Utilized Bokeh's gmap module to create a plot with Google Maps integration
    - Loaded data from 'dvf_gex.csv' into a pandas dataframe
    - Displayed the first rows of the dataframe for verification
    - Defined a function to plot a Google Map with specified latitude, longitude, zoom level, and map type (default: roadmap)
    - Used the function to plot a map centered at latitude and longitude
    - Saved and displayed the plot

---
+++ Constant.py
@@ -0,0 +1,1 @@
+API_Key_Google_Map = "AIzaSyDQ5wRe5Fr5fIY9MIfuOsd8XR8iRZcV0hk"
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pandas as pd
-from bokeh.io import output_notebook
-output_notebook()
-bokeh_width, bokeh_height = 500,400
+import os
+
+from Constant import API_Key_Google_Map
+from bokeh.io import output_file, show
+from bokeh.plotting import gmap
+from bokeh.models import GMapOptions
+
+bokeh_width, bokeh_height = 500, 400
 
 df = pd.read_csv('dvf_gex.csv')
 
 print(df.head())
+
+lat, lng = 46.2437, 6.0251
+
+def plot(lat, lng, zoom=10, map_type='roadmap'):
+    gmap_options = GMapOptions(lat=lat, lng=lng,
+                               map_type=map_type, zoom=zoom)
+    p = gmap(API_Key_Google_Map, gmap_options, title='Pays de Gex',
+             width=bokeh_width, height=bokeh_height)
+    show(p)
+    return p
+
+
+p = plot(lat, lng)
+show(p)
+
